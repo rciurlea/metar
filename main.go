fe12deab07761c09b1a787042c04c402290d38d6
Radu Ciurlea <raduciurlea@gmail.com>
2017-01-11 17:11:44 +0200

    simple fetch

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+const apiURL = "https://aviationweather.gov/adds/dataserver_current/httpparam?datasource=metars&requestType=retrieve&format=xml&mostRecentForEachStation=constraint&hoursBeforeNow=1.25&stationString=LRBS"
+
+type metar struct {
+	rawText string
+}
 
 func main() {
-	fmt.Println("hola world!")
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		log.Fatal("couldn't connect to NOAA service")
+	}
+	defer resp.Body.Close()
+	io.Copy(os.Stdout, resp.Body)
 }
