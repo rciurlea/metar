8e8bc71017ed0a5818586414f6a0a290e411b065
Radu Ciurlea <raduciurlea@gmail.com>
2017-01-13 13:49:16 +0200

    settings

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"path"
 	"sort"
+	"strings"
 	"time"
 
 	"github.com/fatih/color"
 )
 
-const apiURL = "https://aviationweather.gov/adds/dataserver_current/httpparam?datasource=metars&requestType=retrieve&format=xml&mostRecentForEachStation=constraint&hoursBeforeNow=1.25&stationString=LRBS%20LRCT%20LRCK%20LRSB"
+const apiURL = "https://aviationweather.gov/adds/dataserver_current/httpparam?datasource=metars&requestType=retrieve&format=xml&mostRecentForEachStation=constraint&hoursBeforeNow=1.25&stationString="
 
 type metar struct {
 	RawText   string    `xml:"raw_text"`
@@ -26,6 +30,10 @@ type metar struct {
 	QNHHg     float64   `xml:"altim_in_hg"`
 	WX        string    `xml:"wx_string"`
 	FlightCat string    `xml:"flight_category"`
+	SkyCond   []struct {
+		Cover     string `xml:"sky_cover,attr"`
+		CloudBase int    `xml:"cloud_base_ft_agl,attr"`
+	} `xml:"sky_condition"`
 }
 
 type byStation []metar
@@ -35,7 +43,14 @@ func (s byStation) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 func (s byStation) Less(i, j int) bool { return s[i].RawText < s[j].RawText }
 
 func main() {
-	resp, err := http.Get(apiURL)
+
+	airports, err := getSettingsCmdLine()
+	if err != nil {
+		log.Fatal("usage: metar ICAO1 ICAO2 ...")
+	}
+	queryString := apiURL + airports
+
+	resp, err := http.Get(queryString)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -74,7 +89,7 @@ func generateMetars(xmlText []byte) ([]metar, error) {
 func printMetarSimple(m metar) {
 	printer := getColorPrinter(m.FlightCat)
 	printer.Printf("%-4s ", m.FlightCat)
-	fmt.Println(m.RawText, m.Time)
+	fmt.Println(m.RawText)
 }
 
 func printMetarFull(m metar) {
@@ -91,4 +106,34 @@ func getColorPrinter(flightCat string) *color.Color {
 		return color.New(color.FgRed, color.Bold)
 	}
 	return color.New(color.BgBlack)
+}
+
+func getSettingsDotFile() (string, error) {
+	file, err := os.Open(path.Join(os.Getenv("HOME"), ".metar.json"))
+	if err != nil {
+		return "", fmt.Errorf("could not open: %v", err)
+	}
+	defer file.Close()
+
+	contents, err := ioutil.ReadAll(file)
+	if err != nil {
+		return "", fmt.Errorf("can't read: %v", err)
+	}
+
+	type config struct {
+		Stations []string `xml:"stations"`
+	}
+	conf := config{}
+	err = json.Unmarshal(contents, &conf)
+	if err != nil {
+		return "", fmt.Errorf("can't parse JSON: %v", err)
+	}
+	return strings.Join(conf.Stations, "%20"), nil
+}
+
+func getSettingsCmdLine() (string, error) {
+	if !(len(os.Args) >= 2) {
+		return "", fmt.Errorf("no command line args provided")
+	}
+	return strings.Join(os.Args[1:], "%20"), nil
 }
